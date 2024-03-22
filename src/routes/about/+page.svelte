117734e70528fcb739d9b8c5af5409a2b1ea2ee3
chris <abc@abchris.xyz>
2024-03-23 12:45:54 +1300

    feat: add about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,23 @@
+<div class="container">
+	<h2>About</h2>
+	<p>
+		ErgoWatch is a data-aggragation tool for the <a href="https://ergoplatform.org/">Ergo</a> blockchain.
+	</p>
+	<br />
+	<p>
+		The project started in 2021 to illustrate the workings of the <a
+			href="https://docs.ergoplatform.com/uses/sigmausd/">SigmaUSD</a
+		> algorithmic stable coin that had just launched. From there, it evolved to track other aspects of
+		Ergo.
+	</p>
+</div>
+
+<style>
+	.container {
+		margin: auto;
+		max-width: 600px;
+	}
+	a {
+		color: var(--text-color);
+	}
+</style>
