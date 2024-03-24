a9a492fdd947e42fab5b83b0fde2976a8530e6b3
chris <abc@abchris.xyz>
2024-03-25 12:32:43 +1300

    fix: use lower tagline font on smaller screens

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,18 @@
 <div class="tagline">
-	<!-- <div>Ergo Stats & Monitoring<br />Free, Open, Real-Time.</div> -->
 	<div>Ergo Insights.<br />Free, Open, Real-Time.</div>
 </div>
 
 <style>
 	.tagline {
 		margin-top: 10rem;
-		font-size: 32px;
+		font-size: 24px;
 		font-weight: 600;
 		text-align: center;
 		line-height: 1.4em;
+	}
+	@media (min-width: 600px) {
+		.tagline {
+			font-size: 32px;
+		}
 	}
 </style>
